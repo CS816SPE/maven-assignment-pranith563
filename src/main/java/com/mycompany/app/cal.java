2d2e75481838c33ca5f83d5e29ff29092af8ccdd
pranith563 <pranithreddy563@gmail.com>
2019-02-07 08:35:09 +0530

    Update cal.java

--- src/main/java/com/mycompany/app/cal.java
+++ src/main/java/com/mycompany/app/cal.java
@@ -5,8 +5,8 @@ public class cal {
 
     public static void main(String[] args) {
 
-    	float a, b, res;
-    	char choice, ch;
+    	float a=9, b=8, res;
+    	char choice, ch=1;
     	Scanner scan = new Scanner(System.in);
   	do{
     		System.out.println("1. Add Two Numbers");
@@ -15,7 +15,6 @@ public class cal {
     		System.out.println("4. Multiply Two Numbers");
 
     		System.out.println(" Enter your choice");
-			choice = scan.next().charAt(0);
 			switch(choice)
 			{
 				case '1' :  System.out.println(" Enter Two Numbers: ");
