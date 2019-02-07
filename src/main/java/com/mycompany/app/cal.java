4dfbe289d6c2db0e485bf891ceb47c5fa450b2ae
pranith563 <pranithreddy563@gmail.com>
2019-02-07 08:42:10 +0530

    Update cal.java

--- src/main/java/com/mycompany/app/cal.java
+++ src/main/java/com/mycompany/app/cal.java
@@ -5,41 +5,16 @@ public class cal {
 
     public static void main(String[] args) {
 
-    	float a=9, b=8, res;
-    	char choice=1, ch=1;
-  	do{
-    		
-			switch(choice)
-			{
-				case '1' :  System.out.println(" Adding Two Numbers: ");
-							System.out.println("a = "+a);
-					
-							System.out.println("b = "+b);
-							res = a + b;
-							System.out.println(" result = " + res);
-							break;
-
-				case '2' :  System.exit(0);
-							break;
-
-				case '3' :  System.out.println(" Enter Two Numbers: ");
-
-							res = a - b;
-							System.out.println(" result = " + res);
-							break;
-
-				case '4' :  System.out.println(" Enter Two Numbers: ");
-							
-							res = a*b;
-							System.out.println(" result = " + res);
-							break;
-							
-				default  :  System.out.println("Invalid choice");
-							break;
-
-			}
-	}while(choice!=2);
-
+    	float a, b;
+        a = (float)8;
+        b = (float)7;
+        System.out.println( "The input numbers are "+a+" "+b );
+        float res = a+b;
+        System.out.println( "The sum of the numbers are "+res);
+        res = a-b;
+        System.out.println( "The difference is "+res);
+        res = a*b;
+        System.out.println( "The product is "+res);
     	
 
     }
